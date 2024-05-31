1b91ea38f1a69e8652451f458a20e0fef3f02986
JUNGSEUNGKIM <perghost5284@gmail.com>
2024-05-31 16:55:15 +0900

    Initialize project using Create React App

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.awt.image.BufferedImage;
+import java.nio.Buffer;
+import java.util.HashSet;
+import java.util.Scanner;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+
+//        Scanner sc = new Scanner();
+        String str = new String();
+
+        Math.random();
+
+//        BufferedImage
+
+
+
+
+    }
+
+}
